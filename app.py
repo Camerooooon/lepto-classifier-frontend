3737ffe4726492b3b8a02df50906d526a378d70d
Camerooooon <cjpuffnstuff@gmail.com>
2022-07-23 16:34:29 -0700

    Import lepto classifier into project

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, Response
+from leptoclassifier.lepto_classifier import LeptoClassifier
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -11,4 +14,6 @@ def index():
 def submit_data():
     request_data = request.form
     print(request_data);
+    df = pd.DataFrame(request_data.to_dict(flat=False), index=[0])
+    print(df);
     return Response("Ok!", status=200)
