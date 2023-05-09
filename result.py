4379759240f7e8f80bc39cb159a9009513bbb5e9
Camerooooon <cjpuffnstuff@gmail.com>
2023-05-08 19:29:24 -0700

    Fix result not displaying correct value on final pdf

--- result.py
+++ result.py
@@ -1,9 +1,17 @@
 from enum import Enum
 
+def from_prediction(prediction):
+    if (prediction > 0):
+        return Result.POSITIVE
+    elif (prediction == 0):
+        return Result.NEGATIVE
+    else:
+            return Result.INVALID
 class Result(Enum):
-    POSITIVE = 1
-    NEGATIVE = 0
-    INVALID = -1
+    POSITIVE = 1.0
+    NEGATIVE = 0.0
+    INVALID = -1.0
+
 
     def fmt(self):
         if (self == Result.POSITIVE):
